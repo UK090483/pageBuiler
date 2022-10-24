1f6eb10338bb2d03084c907d388a9594ff2a30b2
Konrad Ullrich <43064630+UK090483@users.noreply.github.com>
2022-10-24 15:04:31 +0200

    test

--- PageBuilder/__test__/querytest.ts
+++ PageBuilder/__test__/querytest.ts
@@ -1,14 +1,14 @@
 import { parse, evaluate } from "groq-js";
 
 export const testQuery = async (query: string, dataSet?: any[]) => {
-  let dataset = [
+  let _dataset = [
     { _type: "user", name: "Michael" },
     { _type: "company", name: "Bluth Company" },
   ];
 
   try {
     const tree = parse(query);
-    let value = await evaluate(tree, { dataset });
+    let value = await evaluate(tree, { dataset: dataSet || _dataset });
     let result = await value.get();
     return result;
   } catch (error) {
--- PageBuilderPlugins/SeoPlugin/seoPlugin.test.tsx
+++ PageBuilderPlugins/SeoPlugin/seoPlugin.test.tsx
@@ -1,7 +1,39 @@
-import { testQuery } from "PageBuilder/__test__/querytest";
+import { testQuery } from "../../PageBuilder/__test__/querytest";
 
 import query from "./query";
 
-describe("query should be valid", () => {
-  expect(testQuery(query({}), [])).toBe("");
+const defaultSeo = {
+  metaTitle: "title",
+  metaDesc: "description",
+  shareTitle: "title",
+  shareDesc: "description",
+};
+
+const page = {
+  _type: "page",
+  seo: {
+    metaTitle: "title",
+    metaDesc: "description",
+    shareTitle: "title",
+    shareDesc: "description",
+  },
+};
+
+describe("seo query", () => {
+  it("should be valid", async () => {
+    const res = await testQuery(`*[_type == 'page']{${query({})}}`, []);
+    expect(res).toEqual([]);
+  });
+
+  it("should be ", async () => {
+    const res = await testQuery(`*[_type == 'page'][]{${query({})}}`, [
+      defaultSeo,
+      page,
+    ]);
+    expect(res).toEqual([]);
+  });
+
+  // it("should ", () => {
+  //   expect(testQuery(query({}), [defaultSeo])).resolves.toBe({});
+  // });
 });
